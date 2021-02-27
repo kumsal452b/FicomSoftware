4c637dcfdc2b05fd0eabb5d2322cc0a2eebf9359
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-27 14:24:46 +0300

    some id defined for resize element

--- src/main/java/org/kumsal/ficomSoft/PrimaryController.java
+++ src/main/java/org/kumsal/ficomSoft/PrimaryController.java
@@ -1,12 +1,26 @@
 package org.kumsal.ficomSoft;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    @FXML
+    private ScrollPane scrolpane1;
+    @FXML
+    private AnchorPane main_anchor_pane;
+
+    @FXML
+    void initialize() {
+    scrolpane1.prefWidthProperty().bind(main_anchor_pane.widthProperty());
+        scrolpane1.prefWidthProperty().bind(main_anchor_pane.widthProperty());
     }
 }
