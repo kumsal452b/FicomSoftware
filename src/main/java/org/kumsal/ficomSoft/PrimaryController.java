fec17e808e528e294f14787353c64fc1e81aad41
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-27 17:23:26 +0300

    set animation element

--- src/main/java/org/kumsal/ficomSoft/PrimaryController.java
+++ src/main/java/org/kumsal/ficomSoft/PrimaryController.java
@@ -7,11 +7,13 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class PrimaryController {
 
@@ -34,5 +36,33 @@ public class PrimaryController {
         File file = new File("src/main/resources/org/kumsal/ficomsoft/image/image1.jpg");
         Image image = new Image(file.toURI().toString());
         prim_imageView.setImage(image);
+        prim_imageView.setOnMouseClicked(mouseEvent -> {
+                    setFadeFromAnim();
+                }
+        );
+    }
+    public void changeImage(String path){
+        
+    }
+    public void setFadeFromAnim() {
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(3000));
+        fadeTransition.setNode(prim_imageView);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(actionEvent -> {
+            setFadeToAnim();
+
+        });
+        fadeTransition.play();
+    }
+    public void setFadeToAnim() {
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(3000));
+        fadeTransition.setNode(prim_imageView);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.setOnFinished(actionEvent -> {
+            setFadeFromAnim();
+        });
+        fadeTransition.play();
     }
 }
