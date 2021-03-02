a0c4663ede11c92103685e1929dc99ad901fdfe0
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-02 21:01:40 +0300

    add new module element

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,10 @@ module org.kumsal.ficomSoft {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
+    requires de.jensd.fx.glyphs.commons;
+    requires de.jensd.fx.glyphs.fontawesome;
+
 
     opens org.kumsal.ficomSoft to javafx.fxml;
     exports org.kumsal.ficomSoft;
-
 }
