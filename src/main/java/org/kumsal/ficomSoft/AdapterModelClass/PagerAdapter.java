41257f9e93296251f372380b5a3aebd1c103b3f4
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-05 00:44:05 +0300

    add new pager adapter for pager vıew eleent

---
+++ src/main/java/org/kumsal/ficomSoft/AdapterModelClass/PagerAdapter.java
@@ -0,0 +1,20 @@
+package org.kumsal.ficomSoft.AdapterModelClass;
+
+import javafx.scene.layout.Pane;
+
+public class PagerAdapter extends org.kairos.layouts.PagerAdapter {
+    @Override
+    public String getPageTitle(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object instantiateItem(Pane pane, int i) {
+        return null;
+    }
+}
