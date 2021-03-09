ade1e01ee0f6bbabbcaf6d733d94834be24b0e93
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-09 17:45:09 +0300

    test element have not starter

--- src/main/java/org/kumsal/ficomSoft/MainPageController.java
+++ src/main/java/org/kumsal/ficomSoft/MainPageController.java
@@ -71,7 +71,6 @@ public class MainPageController extends Activity {
     void initialize() throws IOException {
         AnchorPane pane=FXMLLoader.load(getClass().getResource("home.fxml"));
         mainFragment.getChildren().add(pane);
-        main_page_home.setStyle(".mainScreenPainRigth:hover");
+        main_page_home.setId(".mainScreenPainRigth:hover");
     }
-
 }
