b8942d611d466f6aa576258c7654a51b8ff112d8
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-18 19:49:32 +0300

    add new data elemet

--- src/main/java/org/kumsal/ficomSoft/LoadedFile.java
+++ src/main/java/org/kumsal/ficomSoft/LoadedFile.java
@@ -2,9 +2,19 @@ package org.kumsal.ficomSoft;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.kumsal.ficomSoft.MySqlConector.ConnectorMysql;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class LoadedFile {
 
@@ -52,11 +62,37 @@ public class LoadedFile {
 
     @FXML
     private JFXTextField ara;
+    MysqlDataSource dataSource = ConnectorMysql.connect();
+    ObservableList<LoadedFileModel> theFileModel;
 
     @FXML
-    void initialize() {
+    void initialize() throws SQLException {
+        theFileModel= FXCollections.observableArrayList();
+        sira.setCellValueFactory(new PropertyValueFactory<>("sira"));
+        destisno.setCellValueFactory(new PropertyValueFactory<>("destisno"));
+        spdkod.setCellValueFactory(new PropertyValueFactory<>("spdkod"));
+        spdkarsilik.setCellValueFactory(new PropertyValueFactory<>("spdkarsilik"));
+        ozelkod.setCellValueFactory(new PropertyValueFactory<>("ozelkod"));
+        ozelkarsilik.setCellValueFactory(new PropertyValueFactory<>("ozelkarsilik"));
+        klasno.setCellValueFactory(new PropertyValueFactory<>("klasno"));
+        ktarihi.setCellValueFactory(new PropertyValueFactory<>("ktarihi"));
+        aciklama.setCellValueFactory(new PropertyValueFactory<>("aciklama"));
+        yuktarihi.setCellValueFactory(new PropertyValueFactory<>("yuktarihi"));
+        sil.setCellValueFactory(new PropertyValueFactory<>("sil"));
+        desgistir.setCellValueFactory(new PropertyValueFactory<>("desgistir"));
 
-
+        Statement fileList=dataSource.getConnection().createStatement();
+        ResultSet resultSet=fileList.executeQuery("select * from load_flle");
+        LoadedFileModel loadedFile;
+        int sira=1;
+        while (resultSet.next()){
+            loadedFile=new LoadedFileModel(
+                    String.valueOf(sira),
+                    resultSet.getString(2),
+                        
+            );
+            sira++;
+        }
     }
 
 }
