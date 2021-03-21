5b99e30bfef9e82b6bfd1b928ebf10d26abbf1d7
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-21 17:24:52 +0300

    finished model element

--- src/main/java/org/kumsal/ficomSoft/SirtlikCikartma.java
+++ src/main/java/org/kumsal/ficomSoft/SirtlikCikartma.java
@@ -1,4 +1,78 @@
 package org.kumsal.ficomSoft;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXDatePicker;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class SirtlikCikartma {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableView<> table;
+
+    @FXML
+    private TableColumn<?, ?> ısCheck;
+
+    @FXML
+    private TableColumn<?, ?> destisno;
+
+    @FXML
+    private TableColumn<?, ?> birimad;
+
+    @FXML
+    private TableColumn<?, ?> spdkod;
+
+    @FXML
+    private TableColumn<?, ?> spdkarsilik;
+
+    @FXML
+    private TableColumn<?, ?> ozelkod;
+
+    @FXML
+    private TableColumn<?, ?> ozelkarsilik;
+
+    @FXML
+    private TableColumn<?, ?> klasno;
+
+    @FXML
+    private TableColumn<?, ?> ktarihi;
+
+    @FXML
+    private TableColumn<?, ?> aciklama;
+
+    @FXML
+    private TableColumn<?, ?> yuktarihi;
+
+    @FXML
+    private JFXButton yazdir;
+
+    @FXML
+    private JFXCheckBox oprion1;
+
+    @FXML
+    private JFXDatePicker first;
+
+    @FXML
+    private JFXDatePicker seccond;
+
+    @FXML
+    private JFXCheckBox option2;
+
+    @FXML
+    private JFXDatePicker onlyDate;
+
+    @FXML
+    void initialize() {
+
+    }
 }
