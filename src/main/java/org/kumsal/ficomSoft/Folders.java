cd7035e561310c9c04bfaca0c41551d08154ea86
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-24 17:46:46 +0300

    add new model element

--- src/main/java/org/kumsal/ficomSoft/Folders.java
+++ src/main/java/org/kumsal/ficomSoft/Folders.java
@@ -1,5 +1,57 @@
 package org.kumsal.ficomSoft;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
 public class Folders {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField ara;
+
+    @FXML
+    private Pane adminPanel;
+
+    @FXML
+    private JFXTextField destisno_giriniz;
+
+    @FXML
+    private JFXButton ekle;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> sira;
+
+    @FXML
+    private TableColumn<?, ?> destisno;
+
+    @FXML
+    private TableColumn<?, ?> kayıt_tarihi;
+
+    @FXML
+    private TableColumn<?, ?> kayıt_saati;
+
+    @FXML
+    private TableColumn<?, ?> sil;
+
+    @FXML
+    private TableColumn<?, ?> desgistir;
+
+    @FXML
+    void initialize() {
+
+    }
 }
---
+++ src/main/java/org/kumsal/ficomSoft/foldersModel.java
@@ -0,0 +1,75 @@
+package org.kumsal.ficomSoft;
+
+import com.jfoenix.controls.JFXButton;
+
+public class foldersModel {
+
+    private String sira;
+    
+    private String destisno;
+
+    private String yuklemeSaati;
+
+    private String yuktarihi;
+
+    private JFXButton sil;
+
+    private JFXButton desgistir;
+
+    public foldersModel(String sira, String destisno, String yuktarihi,String yuklemeSaati,JFXButton sil, JFXButton desgistir) {
+        this.sira = sira;
+        this.destisno = destisno;
+        this.yuklemeSaati = yuklemeSaati;
+        this.yuktarihi = yuktarihi;
+        this.sil = sil;
+        this.desgistir = desgistir;
+    }
+
+    public String getSira() {
+        return sira;
+    }
+
+    public void setSira(String sira) {
+        this.sira = sira;
+    }
+
+    public String getDestisno() {
+        return destisno;
+    }
+
+    public void setDestisno(String destisno) {
+        this.destisno = destisno;
+    }
+
+    public String getYuklemeSaati() {
+        return yuklemeSaati;
+    }
+
+    public void setYuklemeSaati(String yuklemeSaati) {
+        this.yuklemeSaati = yuklemeSaati;
+    }
+
+    public String getYuktarihi() {
+        return yuktarihi;
+    }
+
+    public void setYuktarihi(String yuktarihi) {
+        this.yuktarihi = yuktarihi;
+    }
+
+    public JFXButton getSil() {
+        return sil;
+    }
+
+    public void setSil(JFXButton sil) {
+        this.sil = sil;
+    }
+
+    public JFXButton getDesgistir() {
+        return desgistir;
+    }
+
+    public void setDesgistir(JFXButton desgistir) {
+        this.desgistir = desgistir;
+    }
+}
